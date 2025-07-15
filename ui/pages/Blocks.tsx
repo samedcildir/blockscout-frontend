29e29cfa79e78abfcc9e303ff48128ddb067f2a2
Alper Gundogdu <mail@alpergundogdu.com>
2025-07-15 09:21:51 +0100

    Revert "removed pending transactions and uncle blocks"
    This reverts commit 68e9d3821ced1f2429201b85d32ee79cb1cace4e.

--- ui/pages/Blocks.tsx
+++ ui/pages/Blocks.tsx
@@ -72,6 +72,7 @@ const BlocksPageContent = () => {
   const tabs: Array<RoutedTab> = [
     { id: 'blocks', title: 'All', component: <BlocksContent type="block" query={ blocksQuery }/> },
     { id: 'reorgs', title: 'Forked', component: <BlocksContent type="reorg" query={ reorgsQuery }/> },
+    { id: 'uncles', title: 'Uncles', component: <BlocksContent type="uncle" query={ unclesQuery }/> },
   ];
 
   return (
